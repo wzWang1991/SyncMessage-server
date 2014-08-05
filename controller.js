0ea06977ebb04f92a12e57fb55410a2f06038026
Weizhen Wang <ww2339@columbia.edu>
2014-08-05 16:05:35 +0800

    Add pages for server.

---
+++ controller.js
@@ -0,0 +1,108 @@
+"use strict";
+var MongoDB = require('./mongo.js')
+
+exports.index = function (req, res) {
+    res.render('index.html',
+    {
+        title : "SyncMessage",
+        session : req.session
+    });
+};
+
+exports.login = function (req, res) {
+    res.render('login.html', {title : "Log in"});
+};
+
+exports.doLogin = function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    if (username == null || password == null) {
+        res.send("Please input correct username and password.");
+    } else {
+        MongoDB.User.findOne({username : username}, function(err, user) {
+            if (err)
+                throw err;
+            else {
+                if (user == null) {
+                    res.render('login.html',
+                        {
+                            title : "Log in",
+                            error : "We can't find you in the database. Please register first."
+                        });
+                } else {
+                    if (user.password != password) {
+                        res.render('login.html',
+                            {
+                                title : "Log in",
+                                error : "Your password is not correct. Please try it again."
+                            });
+                    } else {
+                        req.session.user = username;
+                        req.session.user_id = user._id;
+                        res.redirect('/user');
+                    }
+                }
+            }
+        });
+    }
+};
+
+exports.logout = function (req, res) {
+    req.session.destroy(function () {
+        res.redirect('/');
+    });
+};
+
+exports.userCenter = function (req, res) {
+    var phones = require('./server').getClients(req.session.user);
+    var onlinePhoneId = [ ];
+    if (phones != null) {
+        for (var key in phones) {
+            onlinePhoneId.push(phones[key].phone_id);
+        }
+    }
+    var onlinePhones = [ ];
+    var offlinePhones = [ ];
+    MongoDB.Phone.find({_id: {$in : onlinePhoneId}}, '_id IMEI IMSI unreadMsgs', function (err, phone) {
+        if (err)
+            throw err;
+        else {
+            onlinePhones = phone;
+            MongoDB.Phone.find({_id: {$nin : onlinePhoneId}}, '_id IMEI IMSI unreadMsgs', function (err, phone) {
+                if (err)
+                    throw err;
+                else {
+                    offlinePhones = phone;
+                    res.render('userCenter.html',
+                        {
+                            title : "User Center",
+                            session: req.session,
+                            onlinePhones : onlinePhones,
+                            offlinePhones : offlinePhones
+                        });
+                }
+
+            });
+        }
+    });
+
+};
+
+exports.requiredAuthentication = function requiredAuthentication(req, res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        req.session.error = 'Access denied!';
+        res.redirect('/login');
+    }
+};
+
+exports.sendmsg = function (req, res) {
+    var user = req.session.user;
+    var phone_id = req.body.phoneid;
+    var number = req.body.sendingNumber;
+    var text = req.body.sendingText;
+    // In fact, we need to check if this phone belongs this user.
+    require('./server').sendMsg(user, phone_id, number, text);
+    res.send("Success!")
+}
---
+++ router-config.js
@@ -0,0 +1,34 @@
+'use strict';
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser')
+var controller = require('./controller');
+var session = require('express-session')
+var register = function (app) {
+    app.engine('html', require('ejs').renderFile);
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(session({
+        secret: 'egasseMcnys',
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(express.static(path.join(__dirname, 'public')));
+    // Index page
+    app.get('/', controller.index);
+
+    // Login page
+    app.get('/login', controller.login);
+    app.post('/login', controller.doLogin);
+    app.get('/logout', controller.logout);
+
+    // User center.
+    app.get('/user', controller.requiredAuthentication, controller.userCenter);
+
+    // Sending message
+    app.post('/sendmsg', controller.requiredAuthentication, controller.sendmsg);
+};
+module.exports = {
+    register :  register
+};
